2d734606ac1709ef6b61e7110f89b0458f28c708
Maarten Bos <martin.de.la.selva@gmail.com>
2023-05-16 19:13:27 +0200

    Initial commit

---
+++ src/main/kotlin/PresentationReader.kt
@@ -0,0 +1,13 @@
+package com.squarpoint.cegit.presentation
+
+class PresentationReader(name: String = "") {
+    val fileName: String = name
+    fun openFile() {
+
+    }
+}
+
+fun main(args: Array<String>) {
+    val pReader = PresentationReader("test")
+
+}
---
+++ src/test/kotlin/PresentationReaderTest.kt
@@ -0,0 +1,16 @@
+package com.squarpoint.cegit.presentation
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class PresentationReaderTest {
+    var pReader: PresentationReader? = null
+
+    @BeforeEach
+    fun setUp() {
+        pReader = PresentationReader()
+    }
+
+    @Test
+    fun openFile() {
+    }
+}
